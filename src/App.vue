552815d00fb9412435998b12357f12e5ed171418
abdullahbinazeem <abdullahazeem001@outlook.com>
2025-05-14 23:28:17 -0700

    Create home page with reusable card components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,11 @@
-<template></template>
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+</script>
+
+<template>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+</template>
